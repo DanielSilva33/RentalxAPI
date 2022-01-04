60988a2a4d840ac6c414b11a3a9910758f49f249
Daniel Silva <daniel.oliveiradasilvam@gmail.com>
2022-01-04 09:47:56 -0300

    docker

--- src/database/index.ts
+++ src/database/index.ts
@@ -4,10 +4,11 @@ interface IOptions {
   host: string;
 }
 
-getConnectionOptions().then(options => {
-  const newOptions = options as IOptions;
-  newOptions.host = 'database'; //Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
-  createConnection({
-    ...options,
+getConnectionOptions()
+  .then(options => {
+    const newOptions = options as IOptions;
+    newOptions.host = 'database'; //Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+    createConnection({
+      ...options,
+    });
   });
-});
