649a4bdaa67212cf7041feb918cfac195d9aa8b0
Daniel Silva <daniel.oliveiradasilvam@gmail.com>
2022-01-05 17:07:38 -0300

    Specificarions

--- src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
@@ -4,12 +4,12 @@ import { container } from "tsyringe";
 import { CreateSpecificationUseCases } from "./CreateSpecificationUseCases";
 
 class CreateSpecificationController {
-    handle(request: Request, response: Response) {
+    async handle(request: Request, response: Response): Promise<Response> {
         const { name, description } = request.body;
 
         const createSpecificationUseCases = container.resolve(CreateSpecificationUseCases);
 
-        createSpecificationUseCases.execute({ name, description });
+        await createSpecificationUseCases.execute({ name, description });
 
         return response.status(201).send();
     }
