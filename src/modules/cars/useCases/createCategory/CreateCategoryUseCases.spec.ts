a14dcbe9f8da437bddca433f2ab9a4ed56248e56
Daniel Silva <daniel.oliveiradasilvam@gmail.com>
2022-01-21 16:27:22 -0300

    Test create category

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCases.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCases.spec.ts
@@ -1,11 +1,50 @@
-describe("Create category", () => {
-    
-    it("", () => {
-        const soma = 2 + 2;
+import { AppError } from "../../../../errors/AppError";
+import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory";
+import { CreateCategoryUseCases } from "./CreateCategoryUseCases";
 
-        const result = 4;
+let createCategoryUseCases: CreateCategoryUseCases;
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
 
-        expect(soma).toBe(result);
+describe("Create Category", () => {
+  
+    beforeEach(() => {
+        categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+        createCategoryUseCases = new CreateCategoryUseCases(categoriesRepositoryInMemory);
     });
+
+    it("Should be able to create a new category", async () => {
+        const category = {
+            name: "Category Test",
+            description: "Category description test"
+        }
+
+        await createCategoryUseCases.execute({
+            name: category.name,
+            description: category.description
+        });
+
+        const categoryCreated = await categoriesRepositoryInMemory.findByName(category.name);
+
+        expect(categoryCreated).toHaveProperty("id");
+    });
+
+    it("Should not be able to create a new category with name exists", async () => {
+
+        expect(async () => {
+            const category = {
+                name: "Category Test",
+                description: "Category description test"
+            }
     
+            await createCategoryUseCases.execute({
+                name: category.name,
+                description: category.description
+            });
+    
+            await createCategoryUseCases.execute({
+                name: category.name,
+                description: category.description
+            });
+        }).rejects.toBeInstanceOf(AppError);
+    });
 });
