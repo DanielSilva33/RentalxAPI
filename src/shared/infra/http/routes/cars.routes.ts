ab7110c0a29e99d14af5b5d73a8d8fa515cf945e
Daniel Silva <daniel.oliveiradasilvam@gmail.com>
2022-03-09 12:57:18 -0300

    routes with ensureAuthenticated and ensureAdmin

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { CreateCarController } from "../../../../modules/cars/useCases/createCar/CreateCarController";
+import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const carsRoutes = Router();
 
-const createcarController = new CreateCarController();
+const createCarController = new CreateCarController();
 
 
-carsRoutes.post("/", createcarController.handle);
+carsRoutes.post("/", ensureAuthenticated, ensureAdmin, createCarController.handle);
 
 export { carsRoutes };
