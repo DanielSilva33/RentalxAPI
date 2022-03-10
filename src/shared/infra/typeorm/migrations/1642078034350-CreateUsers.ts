3d0c0a16964564fed9ebceae9bf9760b185309a4
Daniel Silva <daniel.oliveiradasilvam@gmail.com>
2022-03-10 20:16:17 -0300

    create migration rentals

--- src/shared/infra/typeorm/migrations/1642078034350-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1642078034350-CreateUsers.ts
@@ -9,7 +9,8 @@ export class CreateUsers1642078034350 implements MigrationInterface {
                 columns: [
                     {
                         name: "id",
-                        type: "uuid"
+                        type: "uuid",
+                        isPrimary: true
                     },
                     {
                         name: "name",
