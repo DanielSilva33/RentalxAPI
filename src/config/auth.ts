0de868b806c466aca3a07fb22185bd438d01e859
Daniel Silva <daniel.oliveiradasilvam@gmail.com>
2022-04-11 10:53:29 -0300

    deploy config

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,7 +1,7 @@
 export default {
-    secret_token: "9513b5f2bd83102947aba2524c0514dd",
+    secret_token: process.env.SECRET_KEY,
     expires_in_token: "15m",
-    secret_refresh_token: "dceaf6e4a9ca3046d873d90fbec6635f",
+    secret_refresh_token: process.env.secret_refresh_token,
     expires_in_refresh_token: "30d",
     expires_refresh_token_days: 30,
 };
